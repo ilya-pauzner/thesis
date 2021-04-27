f51830aaf3c847d12ea45b164b4d01435cce10ae
ilya-pauzner <ilya-pauzner@yandex.ru>
2021-04-27 21:08:25 +0300

    Adding shrinking.

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ np.random.seed(987654321)
 def calc_load(hosts, vms, mapping):
     load = np.zeros([len(hosts), 2])
     for vm, loc in zip(vms, mapping):
-        load[loc] += vm
+        load[int(loc)] += vm
     return load
 
 
@@ -24,8 +24,8 @@ def migration_score(mapping1, mapping2, ram):
 def random_reorder(hosts, vms, mapping=None):
     # returns mapping, hosts and vms are the same
 
-    init_mapping = np.zeros(len(vms), dtype=np.int)
-    init_resources = np.zeros([len(hosts), 2], dtype=np.int)
+    init_mapping = np.zeros(len(vms))
+    init_resources = np.zeros([len(hosts), 2])
 
     for i, vm in enumerate(vms):
         possible_locations = np.all(init_resources + vm <= hosts, axis=1)
@@ -41,8 +41,8 @@ def random_reorder(hosts, vms, mapping=None):
 def ffd_reorder(hosts, vms, mapping=None):
     # returns mapping, hosts and vms are the same
 
-    new_resources = np.zeros([len(hosts), 2], dtype=np.int)
-    new_mapping = np.zeros(len(vms), dtype=np.int)
+    new_resources = np.zeros([len(hosts), 2])
+    new_mapping = np.zeros(len(vms))
 
     permutation = np.argsort(vms, axis=0)[:, 1][::-1]
     for i in range(len(vms)):
@@ -72,7 +72,7 @@ def migopt_reorder(hosts, vms, mapping, old_mapping):
                 holes.append((j, max_vm))
             holes.append((j, remaining - max_vm * times))
 
-    matrix = np.zeros([len(vms), len(holes)], dtype=np.int)
+    matrix = np.zeros([len(vms), len(holes)])
     for i in range(len(vms)):
         vm = vms[i]
         old_location = old_mapping[i]
@@ -88,7 +88,7 @@ def migopt_reorder(hosts, vms, mapping, old_mapping):
 
     # hole == [where, size]
     row_ind, col_ind = linear_sum_assignment(matrix)
-    updated_mapping = np.zeros(len(vms), dtype=np.int)
+    updated_mapping = np.zeros(len(vms))
     for i in range(len(vms)):
         hole_location, hole_size = holes[col_ind[i]]
         updated_mapping[i] = hole_location
@@ -96,12 +96,17 @@ def migopt_reorder(hosts, vms, mapping, old_mapping):
     return updated_mapping
 
 
+def shrink(vms):
+    multiplier = np.clip(np.random.random([len(vms), 2]) + 0.5, 0.5, 1)
+    return vms * multiplier
+
+
 def main():
     # now it is broken, since arrangements are just too good!
     hosts, vms, init_mapping = create_problem(1, 100)
 
-    hosts = np.array([[100, 200] for _ in range(5)], dtype=np.int)
-    vms = np.array([[np.random.randint(10, 30), np.random.randint(20, 60)] for _ in range(15)], dtype=np.int)
+    hosts = np.array([[100, 200] for _ in range(5)])
+    vms = np.array([[np.random.randint(10, 30), np.random.randint(20, 60)] for _ in range(15)])
     init_mapping = random_reorder(hosts, vms, None)
     assert init_mapping is not None
 
@@ -109,18 +114,23 @@ def main():
     assert np.all(init_resources <= hosts)
     print('Initial active score:', active_score(init_resources))
 
-    new_mapping = ffd_reorder(hosts, vms, init_mapping)
+    new_vms = shrink(vms)
+    print('Shrinking VMs by random factor from 0.5 to 1, in real life this corresponds to real usage statistics:')
+    print('Most customers do not fully consume their quotas - this makes resources overbooking possible.')
+    print('Now, VM requirements are smaller than before, so that we can rearrange them in order to free hosts.')
+
+    new_mapping = ffd_reorder(hosts, new_vms, init_mapping)
     assert new_mapping is not None
-    new_resources = calc_load(hosts, vms, new_mapping)
+    new_resources = calc_load(hosts, new_vms, new_mapping)
     assert np.all(new_resources <= hosts)
     print('FirstFitDecreasing active score:', active_score(new_resources))
-    print('Migration score incurred by transition:', migration_score(init_mapping, new_mapping, vms[:, 1]))
+    print('Migration score incurred by transition:', migration_score(init_mapping, new_mapping, new_vms[:, 1]))
 
-    updated_mapping = migopt_reorder(hosts, vms, new_mapping, init_mapping)
+    updated_mapping = migopt_reorder(hosts, new_vms, new_mapping, init_mapping)
     assert updated_mapping is not None
-    updated_resources = calc_load(hosts, vms, updated_mapping)
+    updated_resources = calc_load(hosts, new_vms, updated_mapping)
     assert np.all(updated_resources <= hosts)
-    print('Migration score after optimisation:', migration_score(init_mapping, updated_mapping, vms[:, 1]))
+    print('Migration score after optimisation:', migration_score(init_mapping, updated_mapping, new_vms[:, 1]))
 
 
 if __name__ == '__main__':
