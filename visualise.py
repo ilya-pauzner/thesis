f0efdd15afd268cd6a8cf3bde443936e3dc63dec
ilya-pauzner <ilya-pauzner@yandex.ru>
2021-05-15 14:31:14 +0300

    Set sizes.

--- visualise.py
+++ visualise.py
@@ -6,6 +6,6 @@ df = pd.read_csv('results.csv')
 algos = {"Initial", "FirstFitDecreasing", "FirstFitDecreasing + migopt", "PyVPSolver", "PyVPSolver + migopt", "Sercon"}
 filtered_df = df[df.algo.isin(algos)]
 filtered_df = filtered_df.assign(abs_task_type=filtered_df["task_type"].abs())
-sns.relplot(data=filtered_df, x="active", y="migr", style="algo", col="abs_task_type", row="shrink")
-sns.relplot(data=filtered_df, x="host_count", y="time", style="algo", col="abs_task_type", row="shrink")
+sns.relplot(data=filtered_df, x="active", y="migr", style="algo", col="abs_task_type", row="shrink", s=100)
+sns.relplot(data=filtered_df, x="host_count", y="time", style="algo", col="abs_task_type", row="shrink", s=100)
 plt.show()
