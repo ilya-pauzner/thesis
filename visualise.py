b99238832e41431660a21a49e13a80cb278d1435
ilya-pauzner <ilya-pauzner@yandex.ru>
2021-05-17 20:31:13 +0300

    Added pictures.

--- visualise.py
+++ visualise.py
@@ -6,6 +6,16 @@ df = pd.read_csv('results.csv')
 algos = {"Initial", "FirstFitDecreasing", "FirstFitDecreasing + migopt", "PyVPSolver", "PyVPSolver + migopt", "Sercon"}
 filtered_df = df[df.algo.isin(algos)]
 filtered_df = filtered_df.assign(abs_task_type=filtered_df["task_type"].abs())
-sns.relplot(data=filtered_df, x="active", y="migr", style="algo", col="abs_task_type", row="shrink", s=100)
-sns.relplot(data=filtered_df, x="host_count", y="time", style="algo", col="abs_task_type", row="shrink", s=100)
-plt.show()
+
+sns.relplot(data=filtered_df[filtered_df["task_type"] > 0], x="active", y="migr", hue="algo", style="algo",
+            row="abs_task_type", s=200)
+plt.savefig("scores_shrink.png")
+sns.relplot(data=filtered_df[filtered_df["task_type"] < 0], x="active", y="migr", hue="algo", style="algo",
+            row="abs_task_type", s=200)
+plt.savefig("scores_fullpack.png")
+sns.relplot(data=filtered_df[filtered_df["task_type"] > 0], x="host_count", y="time", hue="algo", style="algo",
+            row="abs_task_type", s=200)
+plt.savefig("times_shrink.png")
+sns.relplot(data=filtered_df[filtered_df["task_type"] < 0], x="host_count", y="time", hue="algo", style="algo",
+            row="abs_task_type", s=200)
+plt.savefig("times_fullpack.png")
